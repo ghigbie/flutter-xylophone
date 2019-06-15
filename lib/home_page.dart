af2407f755f66e37a96a86d230c4f7a600bfb539
ghigbie <gehigbie@gmail.com>
2019-06-14 21:01:57 -0600

    Add buttons with colors and functionality

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,19 +8,80 @@ class HomeXylophone extends StatefulWidget {
 }
 
 final player = AudioCache();
-void playNote() {
-  player.play('note2.wav'); //audio player knows the location of assets
+void playNote(number) {
+  player.play('note$number.wav'); //audio player knows the location of assets
 }
 
 class _HomeXylophoneState extends State<HomeXylophone> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FlatButton(
-          onPressed: () {
-            playNote();
-          },
-          child: Text('Press me!')),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              playNote(1);
+            },
+            child: Container(color: Colors.red),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              playNote(2);
+            },
+            child: Container(color: Colors.orange),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              playNote(3);
+            },
+            child: Container(color: Colors.yellow),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              playNote(4);
+            },
+            child: Container(color: Colors.green),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              playNote(4);
+            },
+            child: Container(color: Colors.blue),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              playNote(5);
+            },
+            child: Container(color: Colors.indigo),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              playNote(6);
+            },
+            child: Container(color: Colors.purple),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              playNote(7);
+            },
+            child: Container(color: Colors.black),
+          ),
+        ),
+      ],
     );
   }
 }
