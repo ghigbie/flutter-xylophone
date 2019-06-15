4b02f6c809831ca8ae18b22ff5260aa2d3c35aac
ghigbie <gehigbie@gmail.com>
2019-06-14 20:34:07 -0600

    Add audio players package

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 import 'home_page.dart';
 
+import 'package:english_words/english_words.dart';
+
 void main() => runApp(Xylophone());
 
 class Xylophone extends StatelessWidget {
